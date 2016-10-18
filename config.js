dd0affc318c3cebabd316c018543ffc834c6e563
Brian Barrow <briancbarrow@gmail.com>
2016-10-18 12:58:49 +0000

    trying mLab again

--- config.js
+++ config.js
@@ -2,5 +2,5 @@ exports.DATABASE_URL = process.env.DATABASE_URL ||
                         global.DATABASE_URL ||
                         (process.env.NODE_ENV === 'production' ? 
                         'mongodb://localhost/shopping-list' :
-                        'mongodb://mongodb://,briancbarrow>:<code2016>ds061076.mlab.com:61076/mongo-shopping-list');
+                        'mongodb://<briancbarrow>:<code2016>ds061076.mlab.com:61076/mongo-shopping-list');
 exports.PORT = process.env.PORT || 8080;
