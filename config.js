5ff6d8b44fa43c45e69225332895dc60037dc1d4
Brian Barrow <briancbarrow@gmail.com>
2016-10-18 12:42:57 +0000

    trying new mLab

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
                         global.DATABASE_URL ||
                         (process.env.NODE_ENV === 'production' ? 
-                        'mongodb://ds061076.mlab.com:61076/mongo-shopping-list' :
-                        'mongodb://localhost/shopping-list-dev');
+                        'mongodb://localhost/shopping-list' :
+                        'mongodb://mongodb://ds061076.mlab.com:61076/mongo-shopping-list');
 exports.PORT = process.env.PORT || 8080;
