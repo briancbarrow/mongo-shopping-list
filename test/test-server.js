915bbca7f0589bdef9de9f4575076ed24e34122b
Brian Barrow <briancbarrow@gmail.com>
2016-10-19 02:28:35 +0000

    added chai-things

--- test/test-server.js
+++ test/test-server.js
@@ -39,6 +39,7 @@ describe('Shopping List', function() {
                 res.body.should.include({ _id: '57bd1593d8cd9fe199ccee0f', name: 'Broad beans', __v: 0 });
                 res.body.should.include({ _id: '57bd1593d8cd9fe199ccee10', name: 'Tomatoes', __v: 0 });
                 res.body.should.include({ _id: '57bd1593d8cd9fe199ccee11', name: 'Peppers', __v: 0 });
+                done();
             });
     });
     
